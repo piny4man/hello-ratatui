f49c401e4f69393549cd8a349e72fb637f2e56b2
piny4man <development@pinya.dev>
2023-11-28 19:12:33 +0100

    feat: simple hello world in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
+use std::io::{stdout, Result};
+
+fn main() -> Result<()> {
+    let _ = stdout().execute(EnterAlternateScreen);
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     println!("Hello, world!");
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello Piny4s from RataTUI! (press 'q' to quit)")
+                    .yellow()
+                    .on_black(),
+                area,
+            );
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
